6482c2c1bbd7dddf8fdbcc94dcbaeff9d820f809
Josh <Josh@MBP-van-MacBook.hub>
2022-03-21 17:35:30 +1300

    vercel test

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Test from './Test'
+import './App.css';
+
+function App() {
+  return (
+    <Test />
+  );
+}
+
+export default App;
---
+++ src/Test.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Test = () => {
+  return (
+    <div>Hello this is a test</div>
+  )
+}
+
+export default Test
